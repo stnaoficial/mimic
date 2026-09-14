36ee0c15b7ff30242885cced24cb82ab2ccadee7
Pedro Santana <pedro_h_santana@outlook.com>
2026-09-14 20:06:15 -0300

    Fixing initializer debug implementation

--- cmd/init/initializer.go
+++ cmd/init/initializer.go
@@ -18,8 +18,13 @@ func NewInitializer(debug bool) *initializer {
 
 func (i *initializer) Init() error {
 	if startErr := i.start(); startErr != nil {
-		if abortErr := i.abort(); abortErr != nil && i.debug {
-			cli.Logf(cli.LogSeverityError, "Could not abort changes in the current path\n\n")
+		if abortErr := i.abort(); abortErr != nil {
+			// allow debug
+			if i.debug {
+				cli.Logf(cli.LogSeverityError, abortErr.Error())
+			}
+
+			fmt.Printf("Could not abort changes in the current path\n\n")
 		}
 
 		return startErr
